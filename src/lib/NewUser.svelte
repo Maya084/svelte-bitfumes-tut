845ca2da65825784a0d629cee2080d1ad99d13a9
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-27 12:28:29 +0100

    ✨ Feat(modal): add modal

---
+++ src/lib/NewUser.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+</script>
+
+<div class="mt-4">
+  <button
+    on:click={() => (showModal = true)}
+    class="px-4 py-1 bg-blue-500 text-white rounded cursor-pointer"
+    >New User</button
+  >
+</div>
+
+{#if showModal}
+  <Modal on:close={() => (showModal = false)} />
+{/if}
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -3,6 +3,7 @@
   import user2 from "../assets/images/user2.png";
   import user3 from "../assets/images/user3.png";
   import FilterUser from "./FilterUser.svelte";
+  import NewUser from "./NewUser.svelte";
   import User from "./User.svelte";
 
   let users = [
@@ -50,7 +51,10 @@
 <div>
   <h1 class="text-2xl text-center mt-10">List of users</h1>
 
-  <FilterUser on:filter={filter} />
+  <div class="flex justify-between mx-4 items-center">
+    <FilterUser on:filter={filter} />
+    <NewUser />
+  </div>
 
   {#each filteredUsers as user, i (user.id)}
     <User on:remove={remove} {user} {i} />
