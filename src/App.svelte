aa2925ea44148065da2f7cc836faf1baad37af69
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-27 15:23:57 +0100

    ✨ Feat(global): add routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
   import Users from "./lib/Users.svelte";
+  import HomePage from "./lib/HomePage.svelte";
+  import { Router, Route, Link } from "svelte-navigator";
 </script>
 
-<Users />
+<Router>
+  <nav class="p-4 bg-blue-800 text-white">
+    <Link to="/" class="mx-4 cursor-pointer hover:underline">Home</Link>
+    <Link to="/users" class="mx-4 cursor-pointer hover:underline">Users</Link>
+  </nav>
+
+  <Route path="/">
+    <HomePage />
+  </Route>
+  <Route path="/users">
+    <Users />
+  </Route>
+</Router>
---
+++ src/lib/HomePage.svelte
@@ -0,0 +1,17 @@
+<div class="flex">
+  <div class="m-auto my-10">
+    <h1 class="text-4xl">Svelte tutorial</h1>
+    <p class="mt-4 text-xl text-center">
+      I created this by following this tutorial:
+    </p>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/AilOdkZGeOk"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
+</div>
