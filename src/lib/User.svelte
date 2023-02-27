132d6506184595f197e29c404155750d492f42c9
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-27 10:58:23 +0100

    ✨ Feat(user): add user style

---
+++ src/lib/User.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let user;
+  export let i;
+</script>
+
+<div
+  class="flex m-5 bg-white border p-2 roudned-lg shadow-md hover:shadow-lg cursos-pointer items-center"
+>
+  <p class="w-10">{i + 1}</p>
+  <img src={user.image} alt={user.name} class="rounded-full w-14 h-14" />
+  <p class="mx-5 w-20">{user.name}</p>
+  <p class="mx-5 w-40">{user.email}</p>
+</div>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -2,33 +2,39 @@
   import user1 from "../assets/images/user1.png";
   import user2 from "../assets/images/user2.png";
   import user3 from "../assets/images/user3.png";
+  import User from "./User.svelte";
 
   let users = [
     {
+      id: 1,
       image: user1,
       name: "Jane",
       email: "jane@bitfumes.com",
     },
     {
+      id: 2,
       image: user2,
       name: "Daisy",
       email: "daisy@email.com",
     },
     {
+      id: 3,
       image: user3,
       name: "Anna",
       email: "anna@lol.com",
     },
   ];
+
+  const getUsers = () => {
+    return users;
+  };
 </script>
 
 <div>
-  <h1>List of users</h1>
-  {#each users as user}
-    <div>
-      <img src={user.image} alt={user.name} />
-      <p>{user.name}</p>
-      <p>{user.email}</p>
-    </div>
+  <h1 class="text-2xl text-center mt-10">List of users</h1>
+  {#each getUsers() as user, i (user.id)}
+    <User {user} {i} />
+  {:else}
+    <p>No user found!</p>
   {/each}
 </div>
