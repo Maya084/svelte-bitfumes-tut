c13e8b36808ce387a7993fcfa888f551a5b2dff5
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-02-27 11:12:34 +0100

    ✨ Feat(users): add fitler for users

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -10,29 +10,57 @@
       image: user1,
       name: "Jane",
       email: "jane@bitfumes.com",
+      active: true,
     },
     {
       id: 2,
       image: user2,
       name: "Daisy",
       email: "daisy@email.com",
+      active: false,
     },
     {
       id: 3,
       image: user3,
       name: "Anna",
       email: "anna@lol.com",
+      active: true,
     },
   ];
 
-  const getUsers = () => {
-    return users;
+  let filteredUsers = users;
+
+  const filter = (e) => {
+    let status = e.target.value;
+    if (status === "null") {
+      filteredUsers = users;
+      return;
+    }
+
+    status = status === "true";
+
+    filteredUsers = users.filter((user) => user.active === status);
   };
 </script>
 
 <div>
   <h1 class="text-2xl text-center mt-10">List of users</h1>
-  {#each getUsers() as user, i (user.id)}
+
+  <div class="ml-4 mt-4">
+    <label for="user-filer"> Filter User </label>
+    <select
+      name="user-filer"
+      id="user-filer"
+      on:change={filter}
+      class="border rounded-lg px4 py2 ml-4"
+    >
+      <option value={null}>All</option>
+      <option value={true}>Active</option>
+      <option value={false}>Inactive</option>
+    </select>
+  </div>
+
+  {#each filteredUsers as user, i (user.id)}
     <User {user} {i} />
   {:else}
     <p>No user found!</p>
